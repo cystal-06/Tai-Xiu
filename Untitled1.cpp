c4de0a61f8dc35b868f2400c00c31e22ff17682d
Nguyễn Công Đức <Duccongnguyen086@gmail.com>
2024-07-19 13:15:04 +0700

    no

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -5,16 +5,12 @@ int a;
 int n;
 int c;
 int b;
-while(a!=n){
-	cout<<"vui long nhap so nguyen: ";
-	cin>>c;
-	for(int i=2;i<c;i++)
-		if(c%i==0){
-			b=1;
+
 		}
 		else{
 		b=2;
 		}
+		ksfjknfnksdkfmnkanl
 		if (b==1){
 		
 		cout<<"ka so nguyen to\n";}
